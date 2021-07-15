2cdf5fc453a9ff9d97279d2ce1f4ecf82d282a6d
Yohan <cepoangkuningan@gmail.com>
2021-07-15 15:37:00 +0700

    add Category and Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db.models.fields import CharField, TextField
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+class Post(models.Model):
+    category = models.ForeignKey(Category, related_name="posts", on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    intro = models.TextField()
+    body = models.TextField()
+    date_addes = models.DateTimeField(auto_now_add=True)
--- devbeardblog/settings.py
+++ devbeardblog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'devbeardblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'devbeard',
+        'USER': 'adminblog',
+        'PASSWORD': 'adminblog',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
